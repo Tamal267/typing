69c11b8cfd6a4c5e3ebf9654037b3b0f3b1206cc
Tamal267 <68176996+Tamal267@users.noreply.github.com>
2022-08-27 15:39:59 +0600

    some update for more efficiency

--- typing.js
+++ typing.js
@@ -1,20 +1,22 @@
 alert("This project is not for mobile phone or similar devices");
-let wordList=["along","but","what","programming","Bangladesh","Dhaka","city","world","another","three","but","and","out","take","given","next","code","terminal","though","soft","mother","father","sister","brother","however","there","here","studio","back","inflate","remove","handle","without","display","write","page","that","arrow","percent","press","down","bottom","up","font","find","deflect","certain","mouse","event","allow","permission","select","current","different","tab","that","information","called","document","user","random","row","column","animation","timeout","preview","query","outcome","copy","message","multiple","touching","create","which","will","cause","receive","built","something","chapter","process","propagate","fires","window","linux","initialize","action","value","main","dialog","weight","loss","external","inclusive","exclusive","lower","focus","length","ensure","content","presentation","delay","encounter","object","function","suspicious","nothing","attempt","hacking","comments","string","number","text","bollean","greed","dynamic","arguments","refer","parenthesized","group","pattern","match","wrong","right","people","student","college","university","need","topper","means","after","it","used","late","fast","lazy","particular","trial","version","resulting","something","accident","either","neither","else","then","binary","might","obstacle","consume","regular","expression","often","happen","double","concept","chicken","second","left","right","between","character","plural","imagine","real","cannot","word","show","hold","debate","pray","zero","rather","join","split","digital","debug","repetitive","mistake","situation","little","label","whether","auto","standard","probably","example","seeing","few","second","behave","effect","affect","danger","notice","class","routine","immediate","like"];
+
+let wordList=["life","had","many","which","read","close","let","sea","did","number","more","feet","great","only","group","Dhaka","side","eat","letter","give","but","they","it","well","us","later","has","water","take","you","large","down","face","try","or","time","mother","spell","song","old","look","come","can","food","and","every","far","book","back","him","why","so","came","when","his","white","them","in","along","idea","rellay","big","is","say","until","seem","change","call","end","quite","the","began","about","last","set","hard","stop","me","kind","obey","are","over","different","be","both","often","once","talk","start","way","another","good","use","study","my","we","still","above","as","sound","under","said","level","example","next","while","little","always","carry","land","an","miss","right","eye","list","into","follow","not","point","before","after","around","think","up","name","story","add","what","too","home","know","at","because","answer","high","their","went","two","see","one","want","where","girl","enough","found","most","river","America","mile","go","school","do","must","saw","no","here","off","together","will","soon","never","run"];
+console.log(wordList.length);
 function showingWord(wordList){
     let list=[],x;
     for(i=0;i<22;i++){
-        list.push(wordList[Math.ceil(Math.random()*200)]);
+        list.push(wordList[Math.ceil(Math.random()*161)]);
     }
     return list;
 }
 let findList=showingWord(wordList);
 let findList2=findList.join(" ");
-document.getElementById("showText").innerHTML=findList.join(" ");
+document.getElementById("showText").innerHTML=findList2;
 let startBoxTime;
 let l=60;
 let word=[];
 let w=0,count=0,character=0;
-
+console.log(findList2);
 function textShowing(){
     findList=showingWord(wordList);
     findList2=findList.join(" ");
@@ -37,19 +39,21 @@ document.addEventListener('keydown',event=>{
     
     word.push(event.key);
     if(event.key==" "){
+        if(findList[w]==undefined) w++;
         if(findList[w]==document.getElementById("textBox").value.trim()) {
             count++;
-            findList[w]="<span class=\"spanColor1\">"+findList[w]+"</span>";
+            findList[w]=`<span class="spanColor1">${findList[w]}</span>`;
         }
         else{
-            findList[w]="<span class=\"spanColor2\">"+findList[w]+"</span>";
+            findList[w]=`<span class="spanColor2">${findList[w]}</span>`;
         }
         document.getElementById("textBox").value="";
         if(w==21) {
             findList=showingWord(wordList);
             w=-1;
         }
-        document.getElementById("showText").innerHTML=findList.join(" ");     
+        document.getElementById("showText").innerHTML=findList.join(" ");
+        console.log(document.getElementById("showText").innerHTML);     
         w++;
     }
 },false);
@@ -62,7 +66,8 @@ function wpm(){
         document.getElementById("startBox").innerHTML=l; 
         if(l==0) {
             clearInterval(startBoxTime);
-            document.getElementById("showWpm").innerHTML+=document.getElementById("idBox").value+" "+count+" wpm<br>";
+            console.log(count);
+            document.getElementById("showWpm").innerHTML+=document.getElementById("idBox").value+" <span class=\"spanColor1\">"+(count+5)+"</span> <span class=\"spanColor2\">WPM</span><br>";
         }
     }
 }
