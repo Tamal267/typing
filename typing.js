67c3c4d279ad200a6206d4b91d0478dda26ce22e
Tamal267 <68176996+Tamal267@users.noreply.github.com>
2022-08-28 21:49:49 +0600

    Update typing.js

--- typing.js
+++ typing.js
@@ -1,11 +1,13 @@
-alert("This project is not for mobile phone or similar devices");
-
-let wordList=["life","had","many","which","read","close","let","sea","did","number","more","feet","great","only","group","Dhaka","side","eat","letter","give","but","they","it","well","us","later","has","water","take","you","large","down","face","try","or","time","mother","spell","song","old","look","come","can","food","and","every","far","book","back","him","why","so","came","when","his","white","them","in","along","idea","rellay","big","is","say","until","seem","change","call","end","quite","the","began","about","last","set","hard","stop","me","kind","obey","are","over","different","be","both","often","once","talk","start","way","another","good","use","study","my","we","still","above","as","sound","under","said","level","example","next","while","little","always","carry","land","an","miss","right","eye","list","into","follow","not","point","before","after","around","think","up","name","story","add","what","too","home","know","at","because","answer","high","their","went","two","see","one","want","where","girl","enough","found","most","river","America","mile","go","school","do","must","saw","no","here","off","together","will","soon","never","run"];
-console.log(wordList.length);
+/*
+let wordList=["eight","world","class","have","life","had","many","which","read","close","let","sea","did","number","more","feet","great","only","group","Dhaka","side","eat","letter","give","but","they","it","well","us","later","has","water","take","you","large","down","face","try","or","time","mother","spell","song","old","look","come","can","food","and","every","far","book","back","him","why","so","came","when","his","white","them","in","along","idea","really","big","is","say","until","seem","change","call","end","quite","the","began","about","last","set","hard","stop","me","kind","obey","are","over","different","be","both","often","once","talk","start","way","another","good","use","study","my","we","still","above","as","sound","under","said","level","example","next","while","little","always","carry","land","an","miss","right","eye","list","into","follow","not","point","before","after","around","think","up","name","story","add","what","too","home","know","at","because","answer","high","their","went","two","see","one","want","where","girl","enough","found","most","river","Comilla","mile","go","school","do","must","saw","no","here","off","together","will","soon","never","run","ass","ask","any","box","boy","bye","cow","cup","del","dot","fat","fan","fit","fly","foe","fox","fry","gas","fun","fix","hit","hub","hue","ill","lay","lie","leg","new","pay","pen","pet","tea","tip","yet","ride","race","cold","stay","been","bank","chat","each","evil","even","fair","feel","fine","gone","hear","bowl","born","bone","body","burn","bill","bell","bush","busy","code","data","date","dawn","door","disk","dual","ever","else","fish","falt","flow","full","fuel","from","fund","gear","file","hill","hall","harm","inch","lack","lane","knew","iron","item","lost","meet","push","rear","rank","real","pull","poor","plus","plot","pipe","rate","sell","send","self","seek","save","salt","rose","root","roll","step","tank","tape","tech","task","tend","tone","tony","town","tool","wait","wake","walk","wage","warm","wash","wave","wood"];
+*/
+let wordList=["might","could","he","eight","world","class","have","life","had","many","which","read","close","let","sea","did","number","more","feet","great","only","group","Dhaka","side","eat","letter","give","but","they","it","well","us","later","has","water","take","you","large","down","face","try","or","time","mother","spell","song","old","look","come","can","food","and","every","far","book","back","him","why","so","came","when","his","white","them","in","along","idea","really","big","is","say","until","seem","change","call","end","quite","the","began","about","last","set","hard","stop","me","kind","obey","are","over","different","be","both","often","once","talk","start","way","another","good","use","study","my","we","still","above","as","sound","under","said","level","example","next","while","little","always","carry","land","an","miss","right","eye","list","into","follow","not","point","before","after","around","think","up","name","story","add","what","too","home","know","at","because","answer","high","their","went","two","see","one","want","where","girl","enough","found","most","river","Comilla","mile","go","school","do","must","saw","no","here","off","together","will","soon","never","run"];
+let wordListLength=wordList.length;
+console.log(wordListLength);
 function showingWord(wordList){
     let list=[],x;
     for(i=0;i<22;i++){
-        list.push(wordList[Math.ceil(Math.random()*161)]);
+        list.push(wordList[Math.ceil(Math.random()*(wordListLength-1))]);
     }
     return list;
 }
@@ -17,6 +19,21 @@ let l=60;
 let word=[];
 let w=0,count=0,character=0;
 console.log(findList2);
+let bar=1;
+
+//when restart button clicked
+function openCloseBar(){
+    console.log(bar);
+    if(document.getElementById("moreBox").style.display=="none" || bar==1){ 
+        document.getElementById("moreBox").style.display="block";
+        bar =0;
+    }
+    else {
+        document.getElementById("moreBox").style.display="none";
+    }
+}
+
+//Showing words in the box
 function textShowing(){
     findList=showingWord(wordList);
     findList2=findList.join(" ");
@@ -28,17 +45,20 @@ function textShowing(){
     count=0;
     character=0;
     document.getElementById("textBox").value="";
+    document.getElementById("startBox").innerHTML=l;
+    clearInterval(startBoxTime);
 }
 
-
+//when a button form keyboard is pressed
 document.addEventListener('keydown',event=>{
-    if(document.getElementById("textBox").value.trim()==findList2[0] && character==0){
+    if(document.getElementById("textBox").value.trim()!="" && character==0){
         wpm();
         character++;
     }
     
     word.push(event.key);
-    if(event.key==" "){
+
+    if(event.key==" " && document.getElementById("textBox").value.trim()!=""){
         if(findList[w]==undefined) w++;
         if(findList[w]==document.getElementById("textBox").value.trim()) {
             count++;
@@ -58,6 +78,7 @@ document.addEventListener('keydown',event=>{
     }
 },false);
 
+//Showing one minute result (wpm) from typing
 function wpm(){
     startBoxTime=setInterval(minusTime,1000);
 
@@ -67,7 +88,7 @@ function wpm(){
         if(l==0) {
             clearInterval(startBoxTime);
             console.log(count);
-            document.getElementById("showWpm").innerHTML+=document.getElementById("idBox").value+" <span class=\"spanColor1\">"+(count+5)+"</span> <span class=\"spanColor2\">WPM</span><br>";
+            document.getElementById("showWpm").innerHTML+=`<span class="wpmValue">${document.getElementById("idBox").value}</span> <span class="spanColor1">${count+5} </span> <span class="spanColor2">WPM</span><br>`;
         }
     }
 }
